de7cbc911d9fcaa78bbd7ad0c53790a0792dcf0d
BurakBozan <burakbozantr@gmail.com>
2022-07-06 18:56:16 +0900

    Write better code

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function computerPlay() {
 }
 
 function playRound(thePlayerSelection, theComputerSelection) {
-  if (thePlayerSelection === "Rock" && theComputerSelection === "Paper") {
+  /* if (thePlayerSelection === "Rock" && theComputerSelection === "Paper") {
     return "You lose! Paper beats rock!";
   } else if (
     thePlayerSelection === "Rock" &&
@@ -40,9 +40,39 @@ function playRound(thePlayerSelection, theComputerSelection) {
   ) {
     return "You lose! Rock beats scissors!";
   } else {
-    return "It is a tie!";
+    return "It is a tiee!";
+  } */
+  if (thePlayerSelection === "Rock") {
+    if (theComputerSelection === "Paper") {
+      return "You lose! Paper beats rock!";
+    } else if (theComputerSelection === "Scissors") {
+      return "You win! Rock beats scissors!";
+    } else {
+      return "It is a tie!";
+    }
+  } else if (thePlayerSelection === "Paper") {
+    if (theComputerSelection === "Rock") {
+      return "You win! Paper beats rock!";
+    } else if (theComputerSelection === "Scissors") {
+      return "You lose! Scissors beats paper!"
+    } else "It is a tie!"
+  } else if (thePlayerSelection === "Scissors") {
+    if (theComputerSelection === "Rock") {
+      return "You lose! Rock beats scissors!"
+    } else if (theComputerSelection === "Paper") {
+      return "You win! Scissors beats paper!"
+    } else {
+      return "It is a die!"
+    }
   }
 }
 
 const thePlayerSelection = "Rock";
 const theComputerSelection = computerPlay();
+
+/* function game() {
+    for (let i = 0, i < 5; i++) {
+      
+    }
+  
+} */
