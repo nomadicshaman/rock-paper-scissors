070e21ba78ac3c9fd7d56b497a14dbcbb031a9ee
BurakBozan <burakbozantr@gmail.com>
2022-07-09 22:10:19 +0900

    Make the code work as the TOP wants.

--- script.js
+++ script.js
@@ -1,78 +1,65 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
-  let pickOne = ["Rock", "Paper", "Scissors"];
+  let pickOne = ["rock", "paper", "scissors"];
   let randomOne = pickOne[Math.floor(Math.random() * pickOne.length)];
   return randomOne;
 }
 
-function playRound(thePlayerSelection, theComputerSelection) {
-  /* if (thePlayerSelection === "Rock" && theComputerSelection === "Paper") {
-    return "You lose! Paper beats rock!";
-  } else if (
-    thePlayerSelection === "Rock" &&
-    theComputerSelection === "Scissors"
-  ) {
-    return "You win! Rock beats scissors!";
-  } else if (thePlayerSelection === "Rock" && theComputerSelection === "Rock") {
-    return "It is a tie!";
-  } else if (
-    thePlayerSelection === "Paper" &&
-    theComputerSelection === "Rock"
-  ) {
-    return "You win! Paper beats Rock!";
-  } else if (
-    thePlayerSelection === "Paper" &&
-    theComputerSelection === "Scissors"
-  ) {
-    return "You lose! Scissors beats paper!";
-  } else if (
-    thePlayerSelection === "Paper" &&
-    theComputerSelection === "Paper"
-  ) {
-    return "It is a tie!";
-  } else if (
-    thePlayerSelection === "Scissors" &&
-    theComputerSelection === "Paper"
-  ) {
-    return "You win! Scissors beats paper!";
-  } else if (
-    thePlayerSelection === "Scissors" &&
-    theComputerSelection === "Rock"
-  ) {
-    return "You lose! Rock beats scissors!";
-  } else {
-    return "It is a tiee!";
-  } */
-  if (thePlayerSelection === "Rock") {
-    if (theComputerSelection === "Paper") {
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      computerScore++;
       return "You lose! Paper beats rock!";
-    } else if (theComputerSelection === "Scissors") {
+    } else if (computerSelection === "scissors") {
+      playerScore++;
       return "You win! Rock beats scissors!";
     } else {
       return "It is a tie!";
     }
-  } else if (thePlayerSelection === "Paper") {
-    if (theComputerSelection === "Rock") {
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      playerScore++;
       return "You win! Paper beats rock!";
-    } else if (theComputerSelection === "Scissors") {
-      return "You lose! Scissors beats paper!"
-    } else "It is a tie!"
-  } else if (thePlayerSelection === "Scissors") {
-    if (theComputerSelection === "Rock") {
-      return "You lose! Rock beats scissors!"
-    } else if (theComputerSelection === "Paper") {
-      return "You win! Scissors beats paper!"
+    } else if (computerSelection === "scissors") {
+      computerScore++;
+      return "You lose! Scissors beats paper!";
     } else {
-      return "It is a die!"
+      return "It is a tie!";
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      computerScore++;
+      return "You lose! Rock beats scissors!";
+    } else if (computerSelection === "paper") {
+      playerScore++;
+      return "You win! Scissors beats paper!";
+    } else {
+      return "It is a tie!";
     }
   }
 }
 
-const thePlayerSelection = "Rock";
-const theComputerSelection = computerPlay();
 
-/* function game() {
-    for (let i = 0, i < 5; i++) {
-      
-    }
-  
-} */
+
+
+
+
+
+function game() {
+  for (let i=0; i<5; i++) {
+  const computerSelection = computerPlay();
+  const playerSelection = prompt("Choose your weapon!", "Type it bitch!").toLowerCase();
+  playRound(playerSelection, computerSelection);
+}
+if (playerScore === computerScore) {
+  return "It is a fucking tie!";
+} else if (playerScore > computerScore) {
+  return "You fucking won!"
+} else {
+  return "You lost, lol!"
+}
+}
+
+console.log(game())
