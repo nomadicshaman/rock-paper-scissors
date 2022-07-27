915f7fa4866df2680e7a7214cc7071653c129cb9
BurakBozan <burakbozantr@gmail.com>
2022-07-27 18:56:49 +0900

    add ui to the game

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+// variables for storing scores
+
 let playerScore = 0;
 let computerScore = 0;
+
+// creating a function which decides the selection of computer
 
 function computerPlay() {
   let pickOne = ["rock", "paper", "scissors"];
   let randomOne = pickOne[Math.floor(Math.random() * pickOne.length)];
   return randomOne;
 }
+
+// declaring global variables so that i can use these out of functions
+
+let playerSelection;
+let computerSelection;
+
+// creating a function that plays a round, recording the score and returning winner of the round.
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
@@ -37,25 +48,91 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+// button elements that come html
+
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+const playAgain = document.querySelector('#playAgain')
 
 
 
+// creating a function that make the game playable five times without refreshing the game
+// resets the scores after 5 rounds
+playAgain.disabled = true;
 
+function playGame (pick) {
+  computerSelection = computerPlay();
+  
+  resultRound.textContent = playRound(pick, computerSelection);
 
+  humanScore.textContent = "Human  " + playerScore;
+  robotScore.textContent = "Computer " + computerScore;
 
-function game() {
-  for (let i=0; i<5; i++) {
-  const computerSelection = computerPlay();
-  const playerSelection = prompt("Choose your weapon! Rock, Paper or Scissors?", "Type it bitch!").toLowerCase();
-  console.log(playRound(playerSelection, computerSelection));
+  if (playerScore == 5) {
+    resultGame.textContent = "Victory is yours!";
+    resultRound.textContent = ''
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+    playAgain.disabled = false;
+    rockBtn.className = "optionsAfterGame";
+    paperBtn.className = "optionsAfterGame";
+    scissorsBtn.className = "optionsAfterGame";
+    playAgain.className = "optionsAgain"
+  } else if (computerScore == 5) {
+    resultGame.textContent = "You lost to a computer, noob!";
+    resultRound.textContent = ''
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+    playAgain.disabled = false;
+    rockBtn.className = "optionsAfterGame";
+    paperBtn.className = "optionsAfterGame";
+    scissorsBtn.className = "optionsAfterGame";
+    playAgain.className = "optionsAgain"
+  }
 }
-if (playerScore === computerScore) {
-  console.log("It is a fucking draw!");
-} else if (playerScore > computerScore) {
-  console.log("Dat's ma boi! You fucking won the game!");
-} else {
-  console.log("You lost the game, lol!");
-}
-}
 
-console.log(game())
+
+
+
+// restart button
+
+playAgain.addEventListener('click', function() {
+
+  playerScore = 0;
+  computerScore = 0;
+  resultGame.textContent = ''
+  resultRound.textContent = ''
+  humanScore.textContent = "Human " + playerScore;
+  robotScore.textContent = "Computer " + computerScore;
+  rockBtn.disabled = false;
+  paperBtn.disabled = false;
+  scissorsBtn.disabled = false;
+  playAgain.disabled = true;
+  rockBtn.className = "options";
+  paperBtn.className = "options";
+  scissorsBtn.className = "options";
+  playAgain.className = "optionsAfterGame"
+
+
+})
+
+
+// functioning the buttons
+
+rockBtn.addEventListener("click", function () {
+playGame("rock"); 
+});
+
+
+paperBtn.addEventListener("click", function () {
+  playGame("paper");
+});
+
+
+scissorsBtn.addEventListener("click", function () {
+  playGame("scissors");
+});
+
